7d4b755270e5cf3cf30a86beb03a243344f431d7
chanhalee <chan771788@gmail.com>
2024-04-01 16:49:36 +0900

    feat(Budget) 구매 금액에 대한 요구사항 Test 작성 및 구현
    구매 금액에 대한 요구사항 Test 작성 및 구현

---
+++ src/main/java/lotto/Budget.java
@@ -0,0 +1,25 @@
+package lotto;
+
+public class Budget {
+    private int budget;
+
+    public Budget(int budget) {
+        validateBudget(budget);
+
+        this.budget = budget;
+    }
+
+    private static void validateBudget(int budget) {
+        if (budget > 100000) {
+            throw new RuntimeException("구매 금액은 10만원을 초과할 수 없습니다.");
+        }
+
+        if (budget <= 0) {
+            throw new RuntimeException("구매 금액은 양의 정수여야 합니다.");
+        }
+
+        if (budget % 1000 != 0) {
+            throw new RuntimeException("구매 금액은 1000의 배수여야 합니다.");
+        }
+    }
+}
---
+++ src/test/java/lotto/BudgetTest.java
@@ -0,0 +1,28 @@
+package lotto;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BudgetTest {
+    @Test
+    public void 구매_금액은_10만원_이하여야_한다() {
+        Budget budget = new Budget(99000);
+        Assertions.assertThat(budget).isInstanceOf(Budget.class);
+    }
+    @Test
+    public void 구매_금액이_10만원을_초과하면_예외를_던진다() {
+        Assertions.assertThatThrownBy(() -> new Budget(101000)).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void 구매_금액이_음수일_경우_예외를_던진다() {
+        Assertions.assertThatThrownBy(() -> new Budget(0)).isInstanceOf(RuntimeException.class);
+        Assertions.assertThatThrownBy(() -> new Budget(-1000)).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void 구매_금액이_1000의_배수가_아니면_예외를_던진다() {
+        Assertions.assertThatThrownBy(() -> new Budget(99999)).isInstanceOf(RuntimeException.class);
+    }
+
+}
