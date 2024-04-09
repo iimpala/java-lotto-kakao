02b27ab5d38511e938c9ce223c3f46d5d3cadc78
impala <dla4051@naver.com>
2024-04-09 14:15:54 +0900

    refactor(LottoPurchaseBudget): 에러 메시지 수정
    공백 추가

--- src/main/java/lotto/domain/LottoPurchaseBudget.java
+++ src/main/java/lotto/domain/LottoPurchaseBudget.java
@@ -36,7 +36,7 @@ public class LottoPurchaseBudget {
 
     private void validateBudget(int budget) {
         if (budget > MAX_BUDGET_LIMIT) {
-            throw new IllegalArgumentException("구매 금액은" + MAX_BUDGET_LIMIT + "원을 초과할 수 없습니다.");
+            throw new IllegalArgumentException("구매 금액은 " + MAX_BUDGET_LIMIT + "원을 초과할 수 없습니다.");
         }
 
         if (budget <= MIN_BUDGET_LIMIT) {
@@ -44,7 +44,7 @@ public class LottoPurchaseBudget {
         }
 
         if (budget % TICKET_PRICE != 0) {
-            throw new IllegalArgumentException("구매 금액은" + TICKET_PRICE + "의 배수여야 합니다.");
+            throw new IllegalArgumentException("구매 금액은 " + TICKET_PRICE + "의 배수여야 합니다.");
         }
     }
 }
