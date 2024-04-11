41abfeac6728b9f23f90e17edf7567bbc215ff07
impala <dla4051@naver.com>
2024-04-11 10:44:32 +0900

    refactor(LottoTicketSeller): 상수 추출
    한 티켓에 들어가는 로또번호의 개수를 의미하는 숫자를 상수로 추출

--- src/main/java/lotto/domain/LottoTicketSeller.java
+++ src/main/java/lotto/domain/LottoTicketSeller.java
@@ -1,23 +1,24 @@
 package lotto.domain;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class LottoTicketSeller {
+
+    private static final int TICKET_LOTTO_NUMBER_COUNT = 6;
 
     public List<LottoTicket> purchaseAutoLotto(LottoPurchaseBudget budget) {
         int ticketQuantity = budget.getTicketQuantity();
 
-        List<LottoTicket> tickets = new ArrayList<>();
         List<LottoNumber> lottoNumbers = LottoNumber.getValues();
-        for (int i = 0; i < ticketQuantity; i++) {
-            Collections.shuffle(lottoNumbers);
-            tickets.add(new LottoTicket(lottoNumbers.subList(0, 6)));
-        }
-
-        return tickets;
+        return Stream.generate(() -> {
+                    Collections.shuffle(lottoNumbers);
+                    return new LottoTicket(lottoNumbers.subList(0, TICKET_LOTTO_NUMBER_COUNT));
+                })
+                .limit(ticketQuantity)
+                .collect(Collectors.toList());
     }
 
     public List<LottoTicket> purchaseManualLotto(List<List<Integer>> manualLottoNumbers, LottoPurchaseBudget budget) {
